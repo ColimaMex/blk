ac53a62e3ccddc368148f7d44170452db925eae9
ColimaMex <edgar@byblankspace.com>
2024-08-29 09:56:43 +0200

    Update msg.js

--- msg.js
+++ msg.js
@@ -1,15 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var blackoutDiv = document.createElement('div');
-  blackoutDiv.style.position = 'fixed';
-  blackoutDiv.style.top = '0';
-  blackoutDiv.style.left = '0';
-  blackoutDiv.style.width = '100%';
-  blackoutDiv.style.height = '100%';
-  blackoutDiv.style.backgroundColor = 'black';
-  blackoutDiv.style.zIndex = '9999';
-  blackoutDiv.style.pointerEvents = 'none';
-  
-  document.body.style.pointerEvents = 'none';
 
-  document.body.appendChild(blackoutDiv);
-});
