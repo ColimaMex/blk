22d38bc1311f6205d9ad2c6a4ef031120e928461
dev-bs <edgar@byblankspace.com>
2024-08-29 16:12:46 +0200

    Update check-product.js

--- check-product.js
+++ check-product.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-  console.log("test")
+  var blackoutDiv = document.createElement('div');
+  blackoutDiv.style.position = 'fixed';
+  blackoutDiv.style.top = '0';
+  blackoutDiv.style.left = '0';
+  blackoutDiv.style.width = '100%';
+  blackoutDiv.style.height = '100%';
+  blackoutDiv.style.backgroundColor = 'black';
+  blackoutDiv.style.zIndex = '9999'; 
+  blackoutDiv.style.pointerEvents = 'none'; 
+  
+  document.body.style.pointerEvents = 'none';
+
+  document.body.appendChild(blackoutDiv);
 });
